9154ed4861d467d10227a97ab08d688a023a258d
Muharrem Özen <60547236+muharremozen04@users.noreply.github.com>
2024-03-18 21:33:43 +0300

    Add Login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,42 @@
+import "./Login.css";
+
 function Login() {
-  return <div>Login berkan</div>;
+  return (
+    <div className="loginContainer">
+      <form>
+        <h5>
+          SIGN IN
+          <p className="loginContainerParagraf">
+            Enter your credentials to access your account
+          </p>
+        </h5>
+
+        <label>
+          <p className="loginContainerEmail">Email</p>
+          <input
+            className="loginContainerUserName"
+            placeholder="Enter your email"
+            type="text"
+          />
+        </label>
+        <label>
+          <p className="loginContainerPassword">Password</p>
+          <input
+            className="loginContainerPassword2"
+            placeholder="Enter your password"
+            type="password"
+          />
+        </label>
+        <input type="submit" value="SIGN IN" />
+        <p className="loginContainerParagraf2">
+          Forgot your password?
+          <a className="loginContainerAhref" href="">
+            Reset Password
+          </a>
+        </p>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
