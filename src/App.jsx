86d79466679c017f71ca8e7bc7998a9dec0200df
Muharrem Özen <60547236+muharremozen04@users.noreply.github.com>
2024-03-18 18:53:32 +0300

    homa page update

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="home" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="dashboard" element={<DashboardPage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="register" element={<RegisterPage />} />
